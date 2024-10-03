0c572bb3653b292ea8471b31ac18d5589b836f3a
EliasBM98 <elias.ben8ez@gmail.com>
2024-10-03 10:37:55 +0200

    third commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+/*Pseudocodigo
+inicio-funcion1
+    leer array (de primeras puede ser manual, mas adelante dinamico)
+        recorrer array (vueltas definidas)
+        inicio-para i<array.length
+            crear boton (clase, data-atribute, id, data-tag)
+            añadir boton al fragment
+        create element, data atribute, class-list
+        fin-para
+        Montar fragment en el dom
+fin-funcion1
+
+funcion2
+    leer tag 
+
+    leer array fotos
+        recorrer array fotos 
+            recorrer array tag 
+
+        para array fotos
+            recorrer item.tag
+                    escribir nuevo array item.tag=tag
+            fin-para
+        fin-para
+    escribir nuevo array
+fin-funcion2
+
+inicio-funcion3
+    leer array filtrados
+        recorrer array 
+            para-array filtrados 
+                crear div, imagnes, pie de foto... (construir card)
+            incluir targeta en el fragment 
+            fin-para 
+    montar fragment en galeria
+
+
+fin-funcion3*/
